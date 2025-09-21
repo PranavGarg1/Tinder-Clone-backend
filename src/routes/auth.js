f502d7e9440a8ef7ecd040aa5ebae3211a55b859
PranavGarg92 <gargpranav9292@gmail.com>
2025-09-22 00:59:56 +0530

    express-router added

---
+++ src/routes/auth.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { validateSignUpData } = require("../utils/validation");
+const User = require("../models/user");
+const authRouter = express.Router();
+const bcrypt = require("bcrypt");
+
+authRouter.post("/signup", async (req, res) => {
+  //validating data
+  try {
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+    //encrypting password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //saving user to db
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.send("User added Sucessfully");
+  } catch (err) {
+    res.status(500).send("ERROR: " + err);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid password");
+    } else {
+      const token = await user.getJWT();
+      // console.log(token);
+      res.cookie("token", token);
+      res.send("Login Successful");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+module.exports = authRouter;
